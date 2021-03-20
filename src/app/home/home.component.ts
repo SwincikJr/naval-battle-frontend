2f1230fd0ff9e1a2dbca81055d4821bd6160a391
Mauro Swincik Jr <mauro.sergio@itlean.com.br>
2021-03-20 14:57:24 -0300

    Componente de Login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public readonly options = [
+    { label: 'Novo Cadastro' },
+    { label: 'Esqueci minha senha' },
+    { label: 'Contato' }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module'
+import { LoginComponent } from './login/login.component'
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
