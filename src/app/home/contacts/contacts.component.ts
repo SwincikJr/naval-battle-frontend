fc5edbefc16bf554a511027f514399db877b2332
yagogo123 <yagobareia@hotmail.com>
2021-06-22 19:15:11 -0300

    fix selector name

--- src/app/home/contacts/contacts.component.ts
+++ src/app/home/contacts/contacts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contacts',
+  selector: 'contacts-component',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
